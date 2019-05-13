4d3eda5c8d484c4f674657854d04a20ee61bfe70
201502038 <bnm961126@gmail.com>
2019-05-14 00:30:22 +0900

    assign

--- DC02_08_receiver_201502038_KimJeongWoo.py
+++ DC02_08_receiver_201502038_KimJeongWoo.py
@@ -1,21 +1,32 @@
 import socket
+import time
 
 host='0.0.0.0'
-port = 4160
+port = 4220
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind((host, port))
 
+data,addr = s.recvfrom(1024)
+
+size = int(data)
+
 f = open("result_file",'wb')
 data, addr = s.recvfrom(2000)
-
 print('연결됨', addr)
+number = 0
+try:
+   while(data):
+       number += 1
+       proceed = ((1024*number)/size)*100
+       if proceed > 100:
+           print(print("current_size / total_size=",100))
+       else:
+           print("current_size / total_size=",proceed)
+       f.write(data)
+       s.settimeout(2)
+       data,_ =s.recvfrom(1024)
+except OSError:
+             f.close()
+             s.close()
+             print("File Receive End")
 
-while(data):
-    f.write(data)
-    if data:
-      break
-    data,_ =s.recvfrom(1024)
-
-f.close()
-s.close()
-print("FILE RECEIVE END")
--- DC02_08_sender_201502038_KimJeongWoo.py
+++ DC02_08_sender_201502038_KimJeongWoo.py
@@ -3,18 +3,26 @@ import os
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
 host ="127.0.0.1" 
-port = 4160
+port = 4220
 buf = 1024
 
 addr = (host,port) 
 filename = input("input filename:");
 
-f=open(filename, "rb") 
-data=f.read(buf) 
+size = str(os.path.getsize(filename)).encode()
+f=open(filename,"rb")
+data=f.read(buf)
 
+s.sendto(size,(addr))
+number = 0
 while(data):
+    number += 1
+    proceed = ((1024*number)/size)*100
     if(s.sendto(data,addr)):
-        print("sending ...")
+	if proceed>100:
+             print(print("current_size / total_size=",100))
+        else:
+             print("current_size / total_size=",proceed)
         data = f.read(buf) 
 
 s.close() 
